ba186d21db7bea4a6e79c0e2ff6d3a66d3aaa58a
BernardoMorais <bernardomalfredo@hotmail.com>
2018-03-12 19:18:13 -0300

    Atualization with time control on paused.

--- app/src/main/java/com/nothing/bernardo/nothinginternational/MainActivity.java
+++ app/src/main/java/com/nothing/bernardo/nothinginternational/MainActivity.java
@@ -5,7 +5,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.util.Log;
+
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = "MainActivity";
+    private Date currentTime;
+    private String formattedDateOnStart;
+    private long startTime;
+    private String fim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,40 +27,75 @@ public class MainActivity extends AppCompatActivity {
         // Initiate main textview
 
         // initiate view's (imagebutton)
-        ImageButton imgButtonBr = (ImageButton)findViewById(R.id.imageButton3);
-        ImageButton imgButtonIt = (ImageButton)findViewById(R.id.imageButton4);
-        ImageButton imgButtonEs = (ImageButton)findViewById(R.id.imageButton5);
-        ImageButton imgButtonUs = (ImageButton)findViewById(R.id.imageButton6);
+        ImageButton imgButtonBr = findViewById(R.id.imageButton3);
+        ImageButton imgButtonIt = findViewById(R.id.imageButton4);
+        ImageButton imgButtonEs = findViewById(R.id.imageButton5);
+        ImageButton imgButtonUs = findViewById(R.id.imageButton6);
 
         // perform click event on button's
         imgButtonBr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView mainNothing = (TextView)findViewById(R.id.changeNothing);
+                TextView mainNothing = findViewById(R.id.changeNothing);
                 mainNothing.setText(R.string.nada);
             }
         });
         imgButtonIt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView mainNothing = (TextView)findViewById(R.id.changeNothing);
+                TextView mainNothing = findViewById(R.id.changeNothing);
                 mainNothing.setText(R.string.nada);
             }
         });
         imgButtonEs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView mainNothing = (TextView)findViewById(R.id.changeNothing);
+                TextView mainNothing = findViewById(R.id.changeNothing);
                 mainNothing.setText(R.string.nada);
             }
         });
         imgButtonUs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView mainNothing = (TextView)findViewById(R.id.changeNothing);
+                TextView mainNothing = findViewById(R.id.changeNothing);
                 mainNothing.setText(R.string.nada);
             }
         });
     }
-
+    /* (non-Javadoc)
+    * @see android.app.Activity#onStart()
+    * current time is a global variable.
+    * formattedDateOnStart is a global variable to tell the ate on start status.
+    */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentTime = Calendar.getInstance().getTime();
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy-HH:mm:ss");
+        formattedDateOnStart = df.format(currentTime);
+        Log.i(TAG, formattedDateOnStart);
+        setContentView(R.layout.activity_main);
+        TextView datez = findViewById(R.id.date);
+        TextView dates = findViewById(R.id.date2);
+        startTime = System.currentTimeMillis();
+        datez.setText(formattedDateOnStart);
+        dates.setText(fim);
+    }
+    /* (non-Javadoc)
+    * @see android.app.Activity#onPause()
+    * dates is a textview object to set date2 text
+    */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        String formattedDate = df.format(currentTime);
+        Log.i(TAG, formattedDate);
+        long endTime = System.currentTimeMillis();
+        long seconds = (endTime - startTime) / 1000;
+        setContentView(R.layout.activity_main);
+        TextView dates = findViewById(R.id.date2);
+        fim = "Voltou! Uma breve pausa de: " + seconds;
+        dates.setText(fim);
+    }
 }
